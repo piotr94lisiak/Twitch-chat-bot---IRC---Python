49068120f87d1b718f0abc39810d3b63b22afb05
Piotr Lisiak <31732848+piotr94lisiak@users.noreply.github.com>
2018-01-31 22:53:17 +0100

    Add files via upload

--- Twitch Chat Bot  - Python/Settings.py
+++ Twitch Chat Bot  - Python/Settings.py
@@ -4,3 +4,9 @@ PASS = "get your authorization token -> http://twitchapps.com/tmi/"
 NICK = "april_1_"
 CHANNEL = "lee_sek"
 OWNER = "lee_sek"
+
+#RIOT API#
+
+APIKey = str("get your development api key -> https://developer.riotgames.com/")
+summonerName = str("midlane%20kingdom")
+region = str("kr")
